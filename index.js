032d947e5f70d6bf314b3ef869aa2abfb4cd93df
Eduardo Bezerra <ujhafsuyprogramy@gmail.com>
2022-05-11 07:51:34 -0300

    work from my school

---
+++ index.js
@@ -0,0 +1,29 @@
+/*var acc = document.getElementsByClassName('btnReadMore')
+var ler = document.getElementsByClassName('ler')
+var i
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+
+    if (ler[i].style.display === 'block') {
+      ler[i].style.display = 'none'
+    } else {
+      ler[i].style.display = 'block'
+    }
+  })
+}*/
+
+const acc = document.getElementsByClassName('sections')
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
+
+/*const acc = document.querySelector('[ data-js="btnReadMore" ]')
+
+acc.addEventListener('click', e => {
+  console.log(e.target)
+})*/
